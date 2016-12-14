f2a461893d2f8a107e014b287467afb13003354c
linhdantran1509 <linhdantran1509@gmail.com>
2016-12-14 11:50:14 +0100

    Add files via upload

---
+++ IME202/ConnexionJdbcMySql.java
@@ -0,0 +1,51 @@
+package identification;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnexionJdbcMySql {
+
+	private Connection connect; 
+	public static String url ="";
+	public static String user ="";
+	public static String motpasse ="";
+
+	public ConnexionJdbcMySql (String url, String user, String password){ 
+		try {
+			String driverName = "org.gjt.mm.mysql.Driver";
+			Class.forName(driverName);
+		}
+		catch (ClassNotFoundException e){
+			System.out.println("DRIVER JDBC not OK");  
+			System.exit(-1);
+		}
+		try{
+			connect = DriverManager.getConnection(url,user,password);
+		} 
+		catch (SQLException ex){
+			System.out.println("CONNEXION not OK");
+			connect = null;
+			System.exit(-1);
+		}
+	}
+
+	public Connection getConnect(){
+		return connect; 
+	}
+
+	public void close(Connection connect){
+		try {
+			connect.close();
+		} 
+		catch (SQLException ex){
+			System.out.println("SQLException : " + ex.getMessage()); 
+		}
+
+	}
+}
+
+
+
+
+
+
